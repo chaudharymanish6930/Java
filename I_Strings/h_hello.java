4dd0516980b1fede090233c4264927d05b9aa19f
Manish Choudhary <me.developer4u@gmail.com>
2025-04-10 17:06:20 +0530

    Add files via upload

---
+++ I_Strings/h_hello.java
@@ -0,0 +1,10 @@
+package I_Strings;
+
+public class h_hello { public static void main(String[]args){
+    // String is immutable...
+    String str="h";
+        String str1=str+"e";
+        String str2=str1 +"ll";
+    System.out.println(str2);
+}
+}
---
+++ J_String_Builder/f_append.java
@@ -0,0 +1,16 @@
+package J_String_Builder;
+
+public class f_append {
+    public static void main(String[]args){
+        StringBuilder ab=new StringBuilder("h");
+        ab.append("e");
+        System.out.println(ab);
+        ab.append("l");
+        System.out.println(ab);
+        ab.append("l");
+        System.out.println(ab);
+        ab.append("o");
+        System.out.println(ab);
+        System.out.println(ab.length());
+    }
+}
