2886b94847c5439ca6e6508699e2833131c0cd7a
Manish Choudhary <me.developer4u@gmail.com>
2025-08-08 10:10:00 +0530

    Update

---
+++ src/Q_Mini_Project/a_guess_number.java
@@ -0,0 +1,28 @@
+package Q_Mini_Project;
+
+import javax.crypto.spec.PSource;
+import java.util.Scanner;
+public class a_guess_number {
+    public static void main(String[] args){
+        int mynumber = (int)(Math.random()*100);
+
+        Scanner scanner= new Scanner(System.in);
+        int usernumber ;
+
+        do{
+            System.out.println("guess a number: ");
+            usernumber = scanner.nextInt();
+
+            if(mynumber == usernumber){
+                System.out.println("nice guess...");
+                break;
+            }
+            else if(mynumber < usernumber){
+                System.out.println("Number is too large");
+            }
+            else{
+                System.out.println("NUmber is too small");
+            }
+        } while(usernumber >= 0);
+    }
+}
