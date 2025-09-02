fee7bafa36cb78091f099896e889bd48d9cf5307
Manish Choudhary <me.developer4u@gmail.com>
2025-09-02 09:44:15 +0530

    Update

---
+++ src/Q_Programs/a_fibbonaci.java
@@ -0,0 +1,26 @@
+package Q_Programs;
+
+import java.util.*;
+
+public class a_fibbonaci {
+    public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+        System.out.print("enter the number: ");
+    int n = scanner.nextInt();
+    int first =0;
+    int second =1;
+//    if(n==1){
+//        System.out.println("Fibbo of "+n+" :"+first);
+//    }
+//    else if(n==2){
+//        System.out.println("Fibb of "+n+" :"+second);
+//    }
+//    else if(n>2){
+        for(int i=1;i<=n;i++){
+            System.out.print(first+" ");
+            int next = first+second;
+            first = second;
+            second = next;
+        }
+    }
+}
