4a25cedb53f48f08b7d661a5302fd1f1262b756d
Manish Choudhary <me.developer4u@gmail.com>
2025-09-02 09:56:40 +0530

    Update

---
+++ src/Q_Programs/b_SumAllDigit.java
@@ -0,0 +1,21 @@
+package Q_Programs;
+
+import java.util.Scanner;
+
+public class b_SumAllDigit {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("enter the number: ");
+        int digit = scanner.nextInt();
+
+        int sum=0;
+        while(digit>0){
+            int num = digit%10;  // to find the remainder
+            sum +=num;
+            digit = digit/10;   // remove the last digit
+        }
+
+        System.out.println("sum of digit: "+sum);
+    }
+}
