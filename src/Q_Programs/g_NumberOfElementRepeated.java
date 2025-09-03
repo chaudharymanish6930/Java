d74bee0e0159d26d8f55bbb13a571f5d66d4d011
Manish Choudhary <me.developer4u@gmail.com>
2025-09-03 22:01:52 +0530

    Update

---
+++ src/Q_Programs/g_NumberOfElementRepeated.java
@@ -0,0 +1,31 @@
+package Q_Programs;
+
+import java.util.Scanner;
+
+public class g_NumberOfElementRepeated {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        // size of array
+        System.out.print("enter the size of array: ");
+        int n = scanner.nextInt();
+        // create an array
+        int[] arr = new int[n];
+
+        System.out.println("Enter the values: ");
+        for(int i =0;i<n;i++){
+            arr[i]= scanner.nextInt();
+        }
+
+        // how many times the element are comes
+        System.out.print("Enter the finding element: ");
+        int target = scanner.nextInt();
+
+        int count =0;
+        for(int i=0; i<n; i++){
+            if(target == arr[i]){
+                count++;
+            }
+        }
+        System.out.println(target+" element comes times: "+count);
+    }
+}
