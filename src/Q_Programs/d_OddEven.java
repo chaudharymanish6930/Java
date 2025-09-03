719f9dd8cd242e5181d7270067ea348e6ca32f04
Manish Choudhary <me.developer4u@gmail.com>
2025-09-03 21:38:36 +0530

    Update

---
+++ src/Q_Programs/d_OddEven.java
@@ -0,0 +1,35 @@
+package Q_Programs;
+
+import java.util.Scanner;
+
+public class d_OddEven {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String choice;  // outside
+
+        do {
+            System.out.println("hello user...");
+            System.out.print("enter your choice 'yes or no': ");
+            choice = scanner.nextLine();
+
+            switch (choice.toLowerCase()) {
+                case "yes":
+                    System.out.print("enter a number :");
+                    int num = scanner.nextInt();
+                    if (num % 2 == 0) {
+                        System.out.println("the number is even :" + num);
+                    } else {
+                        System.out.println("the numbber is odd :" + num);
+                    }
+                    break;
+
+                case "no":
+                    System.out.println("thank you");
+                    break;
+
+                default:
+                    System.out.println("not valid");
+            }
+        }while (choice.equalsIgnoreCase("yes"));
+    }
+}
---
+++ src/Q_Programs/e_PrimeCheck.java
@@ -0,0 +1,10 @@
+package Q_Programs;
+
+import java.util.Scanner;
+
+public class e_PrimeCheck {
+    public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+
+    }
+}
