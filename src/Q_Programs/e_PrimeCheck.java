56ae2bd8aa4b2a7fe79360e554daa07d58b0e8cb
Manish Choudhary <me.developer4u@gmail.com>
2025-09-04 22:00:47 +0530

    Update

--- src/Q_Programs/e_PrimeCheck.java
+++ src/Q_Programs/e_PrimeCheck.java
@@ -3,9 +3,14 @@ package Q_Programs;
 import java.util.Scanner;
 
 public class e_PrimeCheck {
+    public void CheckAge(int age){
+
+    }
     public static void main(String[] args) {
     Scanner scanner = new Scanner(System.in);
 
         System.out.print("enter the age: ");
+        int age = scanner.nextInt();
+        CheckAge(age);
     }
 }
