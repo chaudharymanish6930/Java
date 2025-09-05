766003006659f70b90ec9742d74f683d98f624af
Manish Choudhary <me.developer4u@gmail.com>
2025-09-05 13:29:14 +0530

    Update

--- src/Q_Programs/e_PrimeCheck.java
+++ src/Q_Programs/e_PrimeCheck.java
@@ -3,13 +3,28 @@ package Q_Programs;
 import java.util.Scanner;
 
 public class e_PrimeCheck {
-    public void CheckAge(int age){
-
+    public static void CheckAge(int age){
+        if(age<=1){
+            System.out.println("not valid or 1 is not a prime number");
+            return;
+        }
+        boolean flag = true;
+        for(int i =2; i<age; i++){
+            if(age%i ==0){
+               flag = false;
+            }
+        }
+        if(flag){
+            System.out.println("it is a prime number");
+        }
+        else{
+            System.out.println("it is not a prime number");
+        }
     }
     public static void main(String[] args) {
     Scanner scanner = new Scanner(System.in);
 
-        System.out.print("enter the age: ");
+        System.out.print("enter a number: ");
         int age = scanner.nextInt();
         CheckAge(age);
     }
