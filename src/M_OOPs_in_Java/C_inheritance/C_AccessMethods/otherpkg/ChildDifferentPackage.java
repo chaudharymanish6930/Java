95275f8e873b238016d0995df3e840eb57266837
Manish Choudhary <me.developer4u@gmail.com>
2025-09-16 11:05:11 +0530

    Update

--- src/M_OOPs_in_Java/C_inheritance/C_AccessMethods/otherpkg/ChildDifferentPackage.java
+++ src/M_OOPs_in_Java/C_inheritance/C_AccessMethods/otherpkg/ChildDifferentPackage.java
@@ -1,5 +1,30 @@
 package M_OOPs_in_Java.C_inheritance.C_AccessMethods.otherpkg;
 
-public class ChildDifferentPackage {
+import M_OOPs_in_Java.C_inheritance.C_AccessMethods.mypkg.Parent;
 
+public class ChildDifferentPackage extends Parent {
+    public ChildDifferentPackage() {
+        super();
+        System.out.println("ChildDifferentPackage constructor");
+    }
+
+    public void accessMethods() {
+        System.out.println("--- Accessing from ChildDifferentPackage (different package) ---");
+
+        publicMethod();        // OK: public is accessible
+        protectedMethod();     // OK: protected is accessible in a subclass even from another package
+        // defaultMethod();    // ERROR if uncommented: default (package-private) NOT accessible outside package
+        // privateMethod();    // ERROR if uncommented: private not accessible
+
+        callPrivate();         // OK: indirect access to private via public wrapper
+
+        // Important note: With a Parent instance in a different package you CANNOT call protected:
+        Parent p = new Parent();
+        // p.protectedMethod(); // ERROR if uncommented: can't access protected on a Parent instance from another package
+    }
+
+    public static void main(String[] args) {
+        ChildDifferentPackage child = new ChildDifferentPackage();
+        child.accessMethods();
+    }
 }
