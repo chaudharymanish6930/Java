fe6120b2e10cb6888d63c9d3f7e855ace15cff43
Manish Choudhary <me.developer4u@gmail.com>
2025-10-01 17:29:58 +0530

    Update

--- src/T_CalenderClass/a_CalendarClass.java
+++ src/T_CalenderClass/a_CalendarClass.java
@@ -7,6 +7,7 @@ public class a_CalendarClass {
         int day = 5, month = 8, year = 2015;
 
         Calendar cal = Calendar.getInstance();
+        System.out.println(cal.getCalendarType());
         cal.set(year, month - 1, day);   // month - 1 because Calendar months are 0-based
 
         String dayOfWeek = cal.getDisplayName(Calendar.DAY_OF_WEEK, Calendar.LONG, Locale.ENGLISH);
--- src/U_DateTime/d_of.java
+++ src/U_DateTime/d_of.java
@@ -1,5 +1,24 @@
 package U_DateTime;
 
-public class d_of {
+import java.time.LocalDateTime;
 
+public class d_of {
+    public static void main(String[] args) {
+
+        LocalDateTime date1 = LocalDateTime.of(2024, 8,13, 0,8,6);
+        System.out.println("Date 1: "+date1);
+
+        LocalDateTime date2 = LocalDateTime.of(2025, 3,22, 0,0,0);
+        System.out.println("Date 2: "+date2);
+
+        if(date1.isBefore(date2)){
+            System.out.println(date1 + " is earlier than "+date2);
+        }
+        else if(date1.isAfter(date2)){
+            System.out.println(date2 + " is later than "+date1);
+        }
+        else if(date1.isEqual(date2)){
+            System.out.println(date1+" is equals to "+date2);
+        }
+    }
 }
