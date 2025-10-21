2d0f566754cf24634a077b079b7518790f8b7046
Manish Choudhary <me.developer4u@gmail.com>
2025-10-21 11:26:04 +0530

    Update

--- src/R_Programs/i_FactorialNumber.java
+++ src/R_Programs/i_FactorialNumber.java
@@ -5,6 +5,7 @@ import java.util.Scanner;
 public class i_FactorialNumber {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a Number :");
         int N = sc.nextInt();
         int product =1;
         for(int i=1; i<=N; i++){
