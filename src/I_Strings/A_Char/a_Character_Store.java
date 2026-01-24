6bb9a87b82ffcdd3e266abc04140e68de293d92a
Manish Choudhary <me.developer4u@gmail.com>
2026-01-24 12:28:37 +0530

    Update code

---
+++ src/I_Strings/A_Char/a_Character_Store.java
@@ -0,0 +1,14 @@
+package I_Strings.A_Char;
+
+public class a_Character_Store {
+    public static void main(String[] args) {
+        char a = 'x';
+        System.out.println(a);
+        char b ='Y';
+        System.out.println(b);
+        char add = '+';
+        System.out.println(add);
+        char per='%';
+        System.out.println(per);
+    }
+}
---
+++ src/K_Operators/D_Arithmetic_Operators/b_Modulo_Operator.java
@@ -0,0 +1,14 @@
+package K_Operators.D_Arithmetic_Operators;
+
+public class b_Modulo_Operator {
+    public static void main(String[] args) {
+        double x=5/2;
+        System.out.println(x); //  2
+        double y=5.0/2;
+        System.out.println(y);  //2.5
+        double z =5.0/2.0;
+        System.out.println(z);
+        double w = 5/2.0;
+        System.out.println(w);
+    }
+}
