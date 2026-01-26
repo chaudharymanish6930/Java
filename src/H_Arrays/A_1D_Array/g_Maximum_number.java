77010651e3aff199664c53cd763b4cf7df92fbd4
Manish Choudhary <me.developer4u@gmail.com>
2026-01-26 14:49:37 +0530

    Update

--- src/H_Arrays/A_1D_Array/g_Maximum_number.java
+++ src/H_Arrays/A_1D_Array/g_Maximum_number.java
@@ -1,7 +1,24 @@
 package H_Arrays.A_1D_Array;
 
+import java.util.Scanner;
+
 public class g_Maximum_number {
     public static void main(String[] args) {
+        Scanner a=new Scanner(System.in);
+        System.out.print("enter the lenght of array: ");
+        int n=a.nextInt();
+        int[] num=new int[n];
 
+        for(int i=0;i<n;i++){
+            System.out.print("Enter element "+i+":");
+            num[i]=a.nextInt();
+        }
+        int max=num[0];
+        for(int i=0; i<num.length; i++){
+            if(num[i]>max){
+                max= num[i];
+            }
+        }
+        System.out.println(max);
     }
 }
