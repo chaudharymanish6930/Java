d16abad4ec1c7e5e568dc8ff024075f5fada20da
Manish Choudhary <me.developer4u@gmail.com>
2026-01-29 16:24:20 +0530

    Update

--- src/H_Arrays/A_1D_Array/i_passing_array_in_method.java
+++ src/H_Arrays/A_1D_Array/i_passing_array_in_method.java
@@ -2,6 +2,12 @@ package H_Arrays.A_1D_Array;
 
 public class i_passing_array_in_method {
     public static void main(String[] args) {
-
+        int[] ab = {12,34,56,96,21};
+        System.out.println(ab[2]);
+        change(ab);
+        System.out.println(ab[2]);
+    }
+    public static void change(int[] arr){
+        arr[2]=30;
     }
 }
