fe4f7cc4841ae53beeee991e0a6ac1d6c5eec013
Manish Choudhary <me.developer4u@gmail.com>
2026-02-12 14:48:53 +0530

    Update

---
+++ src/I_Strings/A_Basics/i_StringFromCharArray.java
@@ -0,0 +1,9 @@
+package I_Strings.A_Basics;
+
+public class i_StringFromCharArray {
+    public static void main(String[] args) {
+        char[] ch ={'c','h','o','u','d','h','a','r','y'};
+        String str = new String(ch);
+        System.out.println(ch);
+    }
+}
