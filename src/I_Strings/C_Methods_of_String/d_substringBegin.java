65ab60aebc3047f282f9cd8a74579eacb1e6af9c
Manish Choudhary <me.developer4u@gmail.com>
2026-02-17 15:40:06 +0530

    update

--- src/I_Strings/C_Methods_of_String/d_substringBegin.java
+++ src/I_Strings/C_Methods_of_String/d_substringBegin.java
@@ -4,5 +4,9 @@ public class d_substringBegin {
     public static void main(String[] args) {
         String str = "hello";
         System.out.println("Substring from index 2: " + str.substring(2)); // Output: llo
+
+        String word = "Programming";
+        System.out.println(word.substring(3));
+        // cut the starting charcter in the given string
     }
 }
