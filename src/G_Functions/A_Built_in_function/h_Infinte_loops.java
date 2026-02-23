bac61ef52061810e23ed058e406681429277ff48
Manish Choudhary <me.developer4u@gmail.com>
2026-02-23 14:37:06 +0530

    Update

---
+++ src/G_Functions/A_Built_in_function/h_Infinte_loops.java
@@ -0,0 +1,20 @@
+package G_Functions.A_Built_in_function;
+
+public class h_Infinte_loops {
+    public static void main(String[] args) {
+        manish();
+    }
+    public static void manish(){
+        System.out.println("Manish");
+        chetan();
+    }
+    public static void chetan(){
+        System.out.println("Chetan");
+        shivam();
+    }
+    public static void shivam(){
+        System.out.println("Shivam");
+        chetan();
+    }
+
+}
