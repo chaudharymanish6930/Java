b6d0f7649895a4c6c41e5a30918af6e3f3d622cd
Manish Choudhary <me.developer4u@gmail.com>
2026-03-14 21:40:27 +0530

    Update

--- src/H_Arrays/A_1D_Array/h_Default_value.java
+++ src/H_Arrays/A_1D_Array/h_Default_value.java
@@ -5,25 +5,35 @@ public class h_Default_value {
         System.out.println("Integer Default value :");
         int[] a = new int[5];
         for(int i=0; i<a.length; i++){
-            System.out.println(a[i]);
+            System.out.print(a[i]+" ");
         }
+        System.out.println();
 
         System.out.println("Double default values :");
         double[] b = new double[5];
         for(int i=0; i<b.length; i++){
-            System.out.println(b[i]);
+            System.out.print(b[i]+" ");
         }
+        System.out.println();
 
         System.out.println("Char default value :");
         char[] c = new char[5];
         for(int i=0; i<c.length; i++){
-            System.out.println(c[i]);
+            System.out.print(c[i]+" ");
         }
+        System.out.println();
 
         System.out.println("Default String Value :");
         String[] s = new String[5];
         for(int i =0; i<s.length; i++){
-            System.out.println(s[i]);
+            System.out.print(s[i]+" ");
+        }
+        System.out.println();
+
+        System.out.println("Default value in Boolean type array:");
+        boolean[] p=new boolean[5];
+        for(int i=0; i<5; i++){
+            System.out.print(p[i]+" ");
         }
     }
 }
